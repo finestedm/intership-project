993dcd56b322e5838e7cc4ce3a8c3a7e37e0e407
Pawel <finestedm@gmail.com>
2023-01-29 20:48:47 +0100

    input for id added, only digits accepted

--- src/App.js
+++ src/App.js
@@ -3,24 +3,42 @@ import React, { useEffect, useState } from 'react';
 import { useLocation, useHistory, useSearchParams } from 'react-router-dom';
 import { Router, Routes, Route, Link, useParams  } from 'react-router-dom';
 import Posts from './Posts';
-import { Container, Button, Row, Col, Stack, Form } from 'react-bootstrap';
+import { Container, Button, Row, Col, Stack, Form, InputGroup } from 'react-bootstrap';
 
 export default function App() {
 
-  const [data, setData] = useState([])
-  const [searchParameters, setSearchParameters] = useSearchParams({page: 1})
-  
-  function handlePageChange(value) {
-    const currentPageNumber = parseInt(searchParameters.get('page'))
-    if (value === 'next') {
-      setSearchParameters({...searchParameters, page: currentPageNumber +1})
-    } else if (value === 'prev' && currentPageNumber > 1) {
-      setSearchParameters({...searchParameters, page: currentPageNumber -1})
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState('')
+  const [searchParameters, setSearchParameters] = useSearchParams({ page: 1, id: '' });
+  const itemsPerPage = 5;
+
+  function setDataDependingOnType(res) {
+    if (Array.isArray(res)) {
+      setData(res)
+    } else {
+      setData([res])
     }
   }
 
   useEffect(() => {
-    const url = `https://reqres.in/api/products?page=${searchParameters.get('page')}`
+    searchParameters.get('id') !== '' && setInput(searchParameters.get('id'))
+  }, [])
+  
+  function handlePageChange(value) {
+    const currentPageNumber = parseInt(searchParameters.get('page'))
+    if (value === 'next') {
+      searchParameters.set('page', currentPageNumber +1)
+    } else if (value === 'prev' && currentPageNumber > 1) {
+      searchParameters.set('page', currentPageNumber -1)
+    } else if (currentPageNumber === NaN) {
+      searchParameters.set('page', 1)
+    }
+    setSearchParameters(searchParameters)
+  }
+
+  useEffect(() => {
+    const url = `https://reqres.in/api/products?id=${searchParameters.get('id')}&per_page=${itemsPerPage}&page=${searchParameters.get('page')}`
+
     const fetchData = async () => {
       const data = await fetch(url);
       const json = await data.json();
@@ -28,25 +46,37 @@ export default function App() {
     }
 
     fetchData()
-      .then((res) => setData(res.data))
+      .then((res) => setDataDependingOnType(res.data))
       .catch(console.error)
   }, [searchParameters])
 
+  useEffect(() => {
+    searchParameters.set('id', input)
+    setSearchParameters(searchParameters)
+  }, [input])
+
   return (
     <div className="App">
-      <Container>
-        <h1>you are on {searchParameters.get('page')} page</h1>
+      <Container className='d-flex flex-column gap-4'>
         <Routes>
           <Route path='/' element={<Posts name={searchParameters.get('page')} data={data} />} />
         </Routes>
         <Stack direction="horizontal" className='d-flex justify-content-between'>
-          <Col><Button onClick={() => handlePageChange('prev')}> Prev </Button></Col>
-          <Col><Button onClick={() => handlePageChange('next')}> Next </Button></Col>
+          <Button onClick={() => handlePageChange('prev')}> Prev </Button>
+          <Button onClick={() => handlePageChange('next')}> Next </Button>
         </Stack>
         <>
-        <Form.Control
-          placeholder='product id'
-        /></>
+        <InputGroup>
+          <Form.Control
+              placeholder='product id'
+              pattern="[0-9]*"
+              value={input}
+              onChange={(e) =>
+                setInput((v) => (e.target.validity.valid ? e.target.value : v))
+              }
+          />
+        </InputGroup>  
+        </>
       </Container>
     </div>
   );
